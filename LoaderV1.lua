08b6cad0e5480686a5256e53fd9ecceb0449d3a2
NoLag-Idss <ezllebrosbrosa@gmail.com>
2025-07-13 11:08:21 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/8X08ScVVyt/0", true))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/MEuBoDvbtC/0", true))()
